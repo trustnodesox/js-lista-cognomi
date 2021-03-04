79ced9c24e80527e22035011d97cdf3b29d05a4a
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-03-04 17:14:52 +0100

    impostazione

---
+++ main.js
@@ -0,0 +1,9 @@
+var cognomi = ["Bianchi", "Rossi", "Perlari", "Lodigiani", "Mastrobattista", "Verdi"]
+cognomi.sort();
+console.log(cognomi);
+
+var domanda = prompt("Inserisci il tuo cognome");
+console.log(domanda);
+cognomi.push (domanda)
+console.log(cognomi);
+cognomi.sort();
